4ebd2d0dc96c223b40f7fc296118a555ec7ee73e
Callum Mellor-Reed <me@callummr.com>
2017-06-22 16:20:20 +0100

    Apply babel-plugin-root-import

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -186,6 +186,14 @@ module.exports = {
           // @remove-on-eject-begin
           babelrc: false,
           presets: [require.resolve('babel-preset-react-app')],
+          plugins: [
+            [
+              require.resolve('babel-plugin-root-import'),
+              {
+                rootPathSuffix: 'src',
+              },
+            ],
+          ],
           // @remove-on-eject-end
           // This is a feature of `babel-loader` for webpack (not Babel itself).
           // It enables caching results in ./node_modules/.cache/babel-loader/
